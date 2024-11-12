7a12345eb6d2d21b123518543081bebfb3258458
Riki Itokazu <itokazuriki@gmail.com>
2024-11-12 16:15:30 -0500

    bool eval

--- src/evaluator/evaluator.go
+++ src/evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
 	"github.com/rikiitokazu/go-interpreter/src/object"
 )
 
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	// Statements
@@ -12,10 +18,17 @@ func Eval(node ast.Node) object.Object {
 		return evalStatements(node.Statements)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
+
 	}
+
 	return nil
 }
 func evalStatements(stmts []ast.Statement) object.Object {
@@ -24,4 +37,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 		result = Eval(statement)
 	}
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
