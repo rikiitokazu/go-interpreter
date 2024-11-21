18913d8d23bd7651a1efc4c30bebc95a3ec5cc88
Riki Itokazu <itokazuriki@gmail.com>
2024-11-21 00:27:51 -0500

    puts

--- src/evaluator/builtins.go
+++ src/evaluator/builtins.go
@@ -1,6 +1,10 @@
 package evaluator
 
-import "github.com/rikiitokazu/go-interpreter/src/object"
+import (
+	"fmt"
+
+	"github.com/rikiitokazu/go-interpreter/src/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": &object.Builtin{
@@ -85,6 +89,14 @@ var builtins = map[string]*object.Builtin{
 			copy(newElements, arr.Elements)
 			newElements[length] = args[1]
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+			return NULL
 		},
 	},
 }
